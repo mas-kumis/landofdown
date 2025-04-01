628db9077fd865f614dff9c7ca90ef42a647dbf0
Mas Kumis Dev <wildanblitar@gmail.com>
2025-04-01 23:28:39 +0700

    shadcn

--- shadcn/components/layout/Navbar.tsx
+++ shadcn/components/layout/Navbar.tsx
@@ -36,16 +36,6 @@ const Navbar = () => {
                 <NavigationMenuLink>WIldan</NavigationMenuLink>
               </NavigationMenuContent>
             </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger className="bg-slate-200">
-                Item One
-              </NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <NavigationMenuLink>WIldan</NavigationMenuLink>
-                <NavigationMenuLink>WIldan</NavigationMenuLink>
-                <NavigationMenuLink>WIldan</NavigationMenuLink>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
       </div>
