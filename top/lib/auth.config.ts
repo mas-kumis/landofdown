2d98e0e8b4bb0997feb9d52095590bbf89ff1369
Mas Kumis Dev <wildanblitar@gmail.com>
2025-05-25 17:15:10 +0700

    auth

--- top/lib/auth.config.ts
+++ top/lib/auth.config.ts
@@ -1,21 +1,4 @@
 import Credentials from "next-auth/providers/credentials";
 import type { NextAuthConfig } from "next-auth";
 
-export default {
-  providers: [
-    Credentials({
-      credentials: {
-        email: {
-          type: "email",
-          label: "Email",
-          placeholder: "johndoe@gmail.com",
-        },
-        password: {
-          type: "password",
-          label: "Password",
-          placeholder: "*****",
-        },
-      },
-    }),
-  ],
-} satisfies NextAuthConfig;
+export default { providers: [Credentials({ })] } satisfies NextAuthConfig;
--- top/lib/middleware.ts
+++
@@ -1,5 +0,0 @@
-import authConfig from "./auth.config";
-import NextAuth from "next-auth";
-
-const { auth } = NextAuth(authConfig);
-export default auth(async function middleware(req: NextRequest) {});
